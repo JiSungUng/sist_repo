e4dd45fb7a5e73f5049f00f76d35cdfa68bceb86
JiSungUng <dnfl345@naver.com>
2023-01-03 11:19:15 +0900

    랭크추가

--- SistJavaProject/src/day0103/arrayscanScore_04.java
+++ SistJavaProject/src/day0103/arrayscanScore_04.java
@@ -6,14 +6,17 @@ public class arrayscanScore_04 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/*먼저 인원수를 입력후 이름, 점수 등을 입력후 출력해 보기.
-		 * 전체 합계와 평균*/
+		 * 전체 합계와 평균
+		 * 등수 구하기 추가*/
 		
 		
 		int people;
 		String []name;
 		int [] score;
+		int [] rank;
 		int tot= 0 ;
 		double avg =0;
+		
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("원원수를 입력하시오.");
@@ -23,6 +26,8 @@ public class arrayscanScore_04 {
 		//인원수 만큼 배열 할당.
 		name = new String[people];
 		score = new int[people];
+		rank = new int[people];
+		
 		
 		for(int i=0;i<people; i++) {
 			System.out.println("이름을 입력하세요");
@@ -34,12 +39,25 @@ public class arrayscanScore_04 {
 		}
 		avg= (double)tot/people;
 		
+		//등수 구하기 (다중 for문)
+		for (int i=0;i<people; i++) {
+			rank[i]=1;
+			for(int j=0;j<people;j++) {
+				//비교되는 대상이 점수가 더 높으면 i 번지 등수를 1 증가시킨다.
+				if(score[i]<score[j]) {
+					rank[i]++;
+				}
+			}
+		}
+
+			
+		
 		//인원수만큼 데이타 입력.
-		System.out.println("번호   이름   점수");
+		System.out.println("번호   이름   점수   등수");
 		
 		for(int i=0; i<people; i++) {
 			
-			System.out.println(i+1+"    "+name[i]+"   "+score[i]);
+			System.out.println(i+1+"    "+name[i]+"   "+score[i]+"  "+rank[i]);
 		}
 		
 		
