47eebc31fe12e2ef995f9d1293a089f91a8ebfd8
JiSungUng <dnfl345@naver.com>
2023-01-03 14:47:20 +0900

    선택정렬 연습

--- SistJavaProject/src/day0103/OrderChangeArray_09.java
+++ SistJavaProject/src/day0103/OrderChangeArray_09.java
@@ -28,7 +28,24 @@ public class OrderChangeArray_09 {
 		}
 		
 		
-		
+		System.out.println("데이터를 오름차순으로 변경하기"); // 중첩 for문 이용.
+		//기준데이터는 0번부터 끝에서 두번째 까지
+		for(int i=0;i<arr.length-1;i++) {
+			
+			//비교하는 데이터는 기준다음값부터 끝까지
+			for(int j=i+1;j<arr.length;j++){
+				if(arr[i]>arr[j]){
+					int temp = arr[i];
+					arr[i]= arr[j];
+					arr[j]=temp;
+				}
+				
+		}
+			
+			}
+		for(int i =0 ;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}	
 	}
 
 }
