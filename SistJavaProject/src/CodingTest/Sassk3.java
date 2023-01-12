aa128508550062c7e1d7c41b333ee07b8518d8a7
JiSungUng <dnfl345@naver.com>
2023-01-12 17:33:22 +0900

    코테

---
+++ SistJavaProject/src/CodingTest/Sassk3.java
@@ -0,0 +1,11 @@
+package CodingTest;
+
+public class Sassk3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(6);
+		System.out.println("dnfl3455");
+	}
+
+}
