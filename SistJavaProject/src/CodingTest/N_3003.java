e9a24b74683f97b898bdf6c470357d9b4b60ef4f
JiSungUng <dnfl345@naver.com>
2023-01-14 22:35:09 +0900

    2023/01/14 공부

---
+++ SistJavaProject/src/CodingTest/N_3003.java
@@ -0,0 +1,26 @@
+package CodingTest;
+
+import java.util.Scanner;
+
+public class N_3003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int king = 1;
+		int queen = 1;
+		int rook = 2;
+		int bishop = 2;
+		int knight = 2;
+		int pawn = 8;
+		
+		System.out.println(king = king - (sc.nextInt()));
+		System.out.println(queen = queen - (sc.nextInt()));
+		System.out.println(rook = rook - (sc.nextInt()));
+		System.out.println(bishop = bishop - (sc.nextInt()));
+		System.out.println(knight = knight - (sc.nextInt()));
+		System.out.println(pawn = pawn - (sc.nextInt()));
+
+	}
+
+}
--- SistJavaProject/src/CodingTest/n_10430.java
+++ SistJavaProject/src/CodingTest/n_10430.java
@@ -7,15 +7,15 @@ public class n_10430 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int a,b,c;
-		a=sc.nextInt();
-		b=sc.nextInt();
-		c=sc.nextInt();
+		int A,B,C;
+		A=sc.nextInt();
+		B=sc.nextInt();
+		C=sc.nextInt();
 		
-		System.out.println((a+b)%c);
-		System.out.println((a%c)+(b%c));
-		System.out.println((a*b)%c);
-		System.out.println((a%c)*(b%c));
+		System.out.println((A+B)%C);
+		System.out.println(((A%C)+(B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C)*(B%C))%C);
 		
 	}
 
