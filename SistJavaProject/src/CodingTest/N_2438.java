8b42211a57a40cd7d374a5975a93c6101d05aae5
JiSungUng <dnfl345@naver.com>
2023-01-17 17:28:50 +0900

    2023/01/17백준

---
+++ SistJavaProject/src/CodingTest/N_2438.java
@@ -0,0 +1,19 @@
+package CodingTest;
+
+import java.util.Scanner;
+
+public class N_2438 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		for(int i=0; i<num;i++) {
+			for(int j=0;j<i+1;j++) {
+				System.out.print("*");
+			}System.out.println();
+		}
+	}
+
+}
