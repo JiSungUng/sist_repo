920f04f153a802e6e45c94d4d070d0c5faba391d
JiSungUng <dnfl345@naver.com>
2023-01-29 14:39:54 +0900

    백준 2920

---
+++ SistJavaProject/src/CodingTest/N_2920.java
@@ -0,0 +1,15 @@
+package CodingTest;
+
+import java.util.Scanner;
+
+public class N_2920 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		int arr []= {1,2,3,4,5,6,7,8};
+		
+		System.out.println(arr[2]);
+	}
+
+}
